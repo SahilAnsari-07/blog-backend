4564e5072378b2a1f0288ce0880db02ea9d9f2d0
Sahil Ansari <sahilansari67891234@gmail.com>
2026-04-05 16:06:15 +0530

    feat: add Comment model

---
+++ src/main/java/com/example/blog/model/Comment.java
@@ -0,0 +1,33 @@
+package com.example.blog.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "comments")
+public class Comment {
+
+    @Id
+    private String id;
+
+    @Field("post_id")
+    private Long postId;
+
+    @Field("user_id")
+    private Long userId;
+
+    private String body;
+
+    private String parentId;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
