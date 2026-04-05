9d4845a427ad4542860e16f112819292be62452f
Sahil Ansari <sahilansari67891234@gmail.com>
2026-04-05 20:21:32 +0530

    feat: add Spring Security and JWT setup

---
+++ src/main/java/com/example/blog/security/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.example.blog.security;
+
+
+import ch.qos.logback.core.encoder.EchoEncoder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+                Jwts.parserBuilder()
+                        .setSigningKey(SECRET_KEY.getBytes())
+                        .build()
+                        .parseClaimsJws(token);
+                return true;
+
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+
+
+}
